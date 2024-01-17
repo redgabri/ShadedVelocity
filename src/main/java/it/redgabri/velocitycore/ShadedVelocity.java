6edf94c773f68c84d69d769ac06c66e35d81c0c1
redgabri <cremonesi.gabriele2009@gmail.com>
2024-01-17 16:10:26 +0100

    [+] Main class

--- src/main/java/it/redgabri/velocitycore/ShadedVelocity.java
+++ src/main/java/it/redgabri/velocitycore/ShadedVelocity.java
@@ -4,19 +4,44 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
 
 @Plugin(
         id = "shadedvelocity",
         name = "ShadedVelocity",
-        version = BuildConstants.VERSION
+        version = BuildConstants.VERSION,
+        authors = {"redgabri"}
 )
 public class ShadedVelocity {
+    private static ProxyServer proxy;
+    private static Logger logger;
+    private static Path dataFolder;
 
     @Inject
-    private Logger logger;
+    public ShadedVelocity(Logger Logger, ProxyServer Proxy, @DataDirectory Path dataDirectory){
+        proxy = Proxy;
+        logger = Logger;
+        dataFolder = dataDirectory;
+    }
+
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+
+    public static ProxyServer getProxy() {
+        return proxy;
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static Path getDataFolder() {
+        return dataFolder;
     }
 }
